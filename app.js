1c70e62951b3b712c70ea88a311421bedb51051b
Samar Shaikh <samar@walkover.in>
2018-08-06 13:11:35 +0530

    samar | added local storage

--- app.js
+++ app.js
@@ -10,30 +10,30 @@ app.get('/', function (req, res) {
     res.sendfile('index.html');
 });
 
-var connectedSockets={};
-var allUsers=[{nickname:"",color:"#000"}];//the initial values of nickname
-io.on('connection',function(socket){
+var connectedSockets = {};
+var allUsers = [{ nickname: "", color: "#000" }];//the initial values of nickname
+io.on('connection', function (socket) {
 
 
-    socket.on('addUser',function(data){ //new users enter the chatroom
-        if(connectedSockets[data.nickname]){//nickname is already occupied
-          socket.emit('userAddingResult',{result:false});
-        }else{
-            socket.emit('userAddingResult',{result:true});
-            socket.nickname=data.nickname;
-            connectedSockets[socket.nickname]=socket;//save each socket instance and send a private message
+    socket.on('addUser', function (data) { //new users enter the chatroom
+        if (connectedSockets[data.nickname]) {//nickname is already occupied
+            socket.emit('userAddingResult', { result: false });
+        } else {
+            socket.emit('userAddingResult', { result: true });
+            socket.nickname = data.nickname;
+            connectedSockets[socket.nickname] = socket;//save each socket instance and send a private message
             allUsers.push(data);
-            socket.broadcast.emit('userAdded',data);//broadcast -> new user welcomed, except for new user
-            socket.emit('allUser',allUsers);//sends all online users to new user
+            socket.broadcast.emit('userAdded', data);//broadcast -> new user welcomed, except for new user
+            socket.emit('allUser', allUsers);//sends all online users to new user
         }
 
     });
 
-    socket.on('addMessage',function(data){ //a user sends a new message
-        if(data.to){//issued to a specific user
-            connectedSockets[data.to].emit('messageAdded',data);
-        }else{
-            socket.broadcast.emit('messageAdded',data);//broadcast messages, except for the user
+    socket.on('addMessage', function (data) { //a user sends a new message
+        if (data.to) {//issued to a specific user
+            connectedSockets[data.to].emit('messageAdded', data);
+        } else {
+            socket.broadcast.emit('messageAdded', data);//broadcast messages, except for the user
         }
 
 
@@ -42,17 +42,17 @@ io.on('connection',function(socket){
 
 
     socket.on('disconnect', function () {  //has user exited ?
-            socket.broadcast.emit('userRemoved', {  //broadcast: has user exit
-                nickname: socket.nickname
-            });
-            for(var i=0;i<allUsers.length;i++){
-                if(allUsers[i].nickname==socket.nickname){
-                    allUsers.splice(i,1);
-                }
+        socket.broadcast.emit('userRemoved', {  //broadcast: has user exit
+            nickname: socket.nickname
+        });
+        for (var i = 0; i < allUsers.length; i++) {
+            if (allUsers[i].nickname == socket.nickname) {
+                allUsers.splice(i, 1);
             }
-            delete connectedSockets[socket.nickname]; //delete the corresponding socket instance
-
         }
+        delete connectedSockets[socket.nickname]; //delete the corresponding socket instance
+
+    }
     );
 });
 
--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -1,22 +1,27 @@
 
-var app=angular.module("chatRoom",[]);
+var app = angular.module("chatRoom", ['LocalStorageModule']);
 
-app.factory('socket', function($rootScope) {
+app.config(function (localStorageServiceProvider) {
+    localStorageServiceProvider
+    .setStorageType('localStorage');
+});
+
+app.factory('socket', function ($rootScope) {
     var socket = io(); //default connection to the server that deploys the site
     return {
-        on: function(eventName, callback) {
-            socket.on(eventName, function() {
+        on: function (eventName, callback) {
+            socket.on(eventName, function () {
                 var args = arguments;
-                $rootScope.$apply(function() {   //manually perform dirty checks
+                $rootScope.$apply(function () {   //manually perform dirty checks
                     callback.apply(socket, args);
                 });
             });
         },
-        emit: function(eventName, data, callback) {
-            socket.emit(eventName, data, function() {
+        emit: function (eventName, data, callback) {
+            socket.emit(eventName, data, function () {
                 var args = arguments;
-                $rootScope.$apply(function() {
-                    if(callback) {
+                $rootScope.$apply(function () {
+                    if (callback) {
                         callback.apply(socket, args);
                     }
                 });
@@ -25,127 +30,128 @@ app.factory('socket', function($rootScope) {
     };
 });
 
-app.factory('randomColor', function($rootScope) {
+app.factory('randomColor', function ($rootScope) {
     return {
-        newColor: function() {
-            return '#'+('00000'+(Math.random()*0x1000000<<0).toString(16)).slice(-6);
+        newColor: function () {
+            return '#' + ('00000' + (Math.random() * 0x1000000 << 0).toString(16)).slice(-6);
         }
     };
 });
 
-app.factory('userService', function($rootScope) {
+app.factory('userService', function ($rootScope, $window, localStorageService) {
     return {
-        get: function(users,nickname) {
-            if(users instanceof Array){
-                for(var i=0;i<users.length;i++){
-                    if(users[i].nickname===nickname){
+        get: function (users, nickname) {
+            if (users instanceof Array) {
+                localStorageService.set('users', users);
+                for (var i = 0; i < users.length; i++) {
+                    if (users[i].nickname === nickname) {
                         return users[i];
                     }
                 }
-            }else{
+            } else {
                 return null;
             }
         }
     };
 });
 
-app.controller("chatCtrl",['$scope','socket','randomColor','userService',function($scope,socket,randomColor,userService){
-    var messageWrapper= $('.message-wrapper');
-    $scope.hasLogined=false;
-    $scope.receiver="";//the default is group chat
-    $scope.publicMessages=[];//group chat message
-    $scope.privateMessages={};//private message
-    $scope.messages=$scope.publicMessages;//show group chat by default
-    $scope.users=[];//
-    $scope.color=randomColor.newColor();//current user avatar color
-    $scope.login=function(){   //log in to chat room
-        socket.emit("addUser",{nickname:$scope.nickname,color:$scope.color});
+app.controller("chatCtrl", ['$scope', 'socket', 'randomColor', 'userService', 'localStorageService', function ($scope, socket, randomColor, userService, localStorageService) {
+    var messageWrapper = $('.message-wrapper');
+    $scope.hasLogined = false;
+    $scope.receiver = "";//the default is group chat
+    $scope.publicMessages = [];//group chat message
+    $scope.privateMessages = {};//private message
+    $scope.messages = $scope.publicMessages;//show group chat by default
+    $scope.users = [];//
+    $scope.color = randomColor.newColor();//current user avatar color
+    $scope.login = function () {   //log in to chat room
+        socket.emit("addUser", { nickname: $scope.nickname, color: $scope.color });
     }
-    $scope.scrollToBottom=function(){
+    $scope.scrollToBottom = function () {
         messageWrapper.scrollTop(messageWrapper[0].scrollHeight);
     }
 
-    $scope.postMessage=function(){
-        var msg={text:$scope.words,type:"normal",color:$scope.color,from:$scope.nickname,to:$scope.receiver};
-        var rec=$scope.receiver;
-        if(rec){  //personal communication
-           if(!$scope.privateMessages[rec]){
-               $scope.privateMessages[rec]=[];
-           }
+    $scope.postMessage = function () {
+        var msg = { text: $scope.words, type: "normal", color: $scope.color, from: $scope.nickname, to: $scope.receiver };
+        var rec = $scope.receiver;
+        if (rec) {  //personal communication
+            if (!$scope.privateMessages[rec]) {
+                $scope.privateMessages[rec] = [];
+            }
             $scope.privateMessages[rec].push(msg);
-        }else{ //group chat
+        } else { //group chat
             $scope.publicMessages.push(msg);
         }
-        $scope.words="";
-        if(rec!==$scope.nickname) { //exclude the situation: send to yourself
+        $scope.words = "";
+        if (rec !== $scope.nickname) { //exclude the situation: send to yourself
             socket.emit("addMessage", msg);
         }
     }
-    $scope.setReceiver=function(receiver){
-        $scope.receiver=receiver;
-        if(receiver){ //private message receiver
-            if(!$scope.privateMessages[receiver]){
-                $scope.privateMessages[receiver]=[];
+    $scope.setReceiver = function (receiver) {
+        $scope.receiver = receiver;
+        if (receiver) { //private message receiver
+            if (!$scope.privateMessages[receiver]) {
+                $scope.privateMessages[receiver] = [];
             }
-            $scope.messages=$scope.privateMessages[receiver];
-        }else{//broadcast
-            $scope.messages=$scope.publicMessages;
+            $scope.messages = $scope.privateMessages[receiver];
+        } else {//broadcast
+            $scope.messages = $scope.publicMessages;
         }
-        var user=userService.get($scope.users,receiver);
-        if(user){
-            user.hasNewMessage=false;
+        var user = userService.get($scope.users, receiver);
+        if (user) {
+            user.hasNewMessage = false;
         }
     }
 
     //received login
-    socket.on('userAddingResult',function(data){
-        if(data.result){
-            $scope.userExisted=false;
-            $scope.hasLogined=true;
-        }else{//nickname is occupied
-            $scope.userExisted=true;
+    socket.on('userAddingResult', function (data) {
+        if (data.result) {
+            $scope.userExisted = false;
+            $scope.hasLogined = true;
+        } else {//nickname is occupied
+            $scope.userExisted = true;
         }
     });
 
     //new user added
-    socket.on('userAdded', function(data) {
-        if(!$scope.hasLogined) return;
-        $scope.publicMessages.push({text:data.nickname,type:"welcome"});
+    socket.on('userAdded', function (data) {
+        if (!$scope.hasLogined) return;
+        $scope.publicMessages.push({ text: data.nickname, type: "welcome" });
         $scope.users.push(data);
     });
 
     //received online user message
-    socket.on('allUser', function(data) {
-        if(!$scope.hasLogined) return;
-        $scope.users=data;
+    socket.on('allUser', function (data) {
+        if (!$scope.hasLogined) return;
+        $scope.users = data;
     });
 
     //received user exit message
-    socket.on('userRemoved', function(data) {
-        if(!$scope.hasLogined) return;
-        $scope.publicMessages.push({text:data.nickname,type:"bye"});
-        for(var i=0;i<$scope.users.length;i++){
-            if($scope.users[i].nickname==data.nickname){
-                $scope.users.splice(i,1);
+    socket.on('userRemoved', function (data) {
+        if (!$scope.hasLogined) return;
+        $scope.publicMessages.push({ text: data.nickname, type: "bye" });
+        for (var i = 0; i < $scope.users.length; i++) {
+            if ($scope.users[i].nickname == data.nickname) {
+                $scope.users.splice(i, 1);
                 return;
             }
         }
     });
 
     //received new message
-    socket.on('messageAdded', function(data) {
-        if(!$scope.hasLogined) return;
-        if(data.to){ //personal communication
-            if(!$scope.privateMessages[data.from]){
-                $scope.privateMessages[data.from]=[];
+    socket.on('messageAdded', function (data) {
+        if (!$scope.hasLogined) return;
+        if (data.to) { //personal communication
+            if (!$scope.privateMessages[data.from]) {
+                $scope.privateMessages[data.from] = [];
             }
             $scope.privateMessages[data.from].push(data);
-        }else{
+        } else {
             $scope.publicMessages.push(data);
         }
-        var fromUser=userService.get($scope.users,data.from);
-        var toUser=userService.get($scope.users,data.to);
-        if($scope.receiver!==data.to) {//prompt for new messages when the party is not chatting
+        var fromUser = userService.get($scope.users, data.from);
+        var toUser = userService.get($scope.users, data.to);
+        if ($scope.receiver !== data.to) {//prompt for new messages when the party is not chatting
             if (fromUser && toUser.nickname) {
                 fromUser.hasNewMessage = true;//personal communication
             } else {
@@ -158,36 +164,36 @@ app.controller("chatCtrl",['$scope','socket','randomColor','userService',functio
 
 }]);
 
-app.directive('message', ['$timeout',function($timeout) {
+app.directive('message', ['$timeout', function ($timeout) {
     return {
         restrict: 'E',
         templateUrl: 'message.html',
-        scope:{
-            info:"=",
-            self:"=",
-            scrolltothis:"&"
+        scope: {
+            info: "=",
+            self: "=",
+            scrolltothis: "&"
         },
-        link:function(scope, elem, attrs){
-                scope.time=new Date();
-                $timeout(scope.scrolltothis);
-                $timeout(function(){
-                    elem.find('.avatar').css('background',scope.info.color);
-                });
+        link: function (scope, elem, attrs) {
+            scope.time = new Date();
+            $timeout(scope.scrolltothis);
+            $timeout(function () {
+                elem.find('.avatar').css('background', scope.info.color);
+            });
         }
     };
 }])
-    .directive('user', ['$timeout',function($timeout) {
+    .directive('user', ['$timeout', function ($timeout) {
         return {
             restrict: 'E',
             templateUrl: 'user.html',
-            scope:{
-                info:"=",
-                iscurrentreceiver:"=",
-                setreceiver:"&"
+            scope: {
+                info: "=",
+                iscurrentreceiver: "=",
+                setreceiver: "&"
             },
-            link:function(scope, elem, attrs,chatCtrl){
-                $timeout(function(){
-                    elem.find('.avatar').css('background',scope.info.color);
+            link: function (scope, elem, attrs, chatCtrl) {
+                $timeout(function () {
+                    elem.find('.avatar').css('background', scope.info.color);
                 });
             }
         };
